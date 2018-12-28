d1c2576cae6964c79f18eb915b4d9f189670bda8
samigharbi <sami.gharbi@gmail.com>
2018-12-28 23:10:15 +0100

    update

---
+++ src/com/mow_it_now/controlers/Loader.java
@@ -0,0 +1,11 @@
+package com.mow_it_now.controlers;
+
+import java.io.IOException;
+
+import com.mow_it_now.models.Pelouse;
+
+public interface Loader {
+ 
+
+  Pelouse createPelouse() throws IOException;
+}
